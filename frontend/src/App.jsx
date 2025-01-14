ddcd5fb013ae9ff4e1df873d072468b6d6e96d02
labdhishah812 <shahlabdhi9720@gmail.com>
2025-01-14 12:12:28 +0530

    project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-red-100 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-500">Hello, Tailwind CSS with Vite!</h1>
+    </div>
+  );
+}
+
+export default App
